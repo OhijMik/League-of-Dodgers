d8678d9710e6a9b50a0d1046d39e07b13a7d1177
OhijMik <jihok148@gmail.com>
2024-06-19 15:04:33 -0400

    Added champion movement
    - Added the champion movement so that it moves towards the mouse position when clicked

--- champion.gd
+++ champion.gd
@@ -5,19 +5,25 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var direction
+var mouse_pos
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction
+	
 	if Input.is_action_just_pressed("move"):
-		direction = get_viewport().get_mouse_position().normalized()
-		
+		mouse_pos = get_viewport().get_mouse_position()
+		direction = (mouse_pos - position).normalized()
+
 	if direction:
-		velocity = direction * SPEED
-	#else:
-		#velocity = move_toward(velocity.x, 0, SPEED)
+		if position.x > mouse_pos.x - 10 and position.x < mouse_pos.x + 10 \
+	   		and position.y > mouse_pos.y - 10 and position.y < mouse_pos.y + 10:
+			velocity = Vector2.ZERO
+			direction = null
+		else:
+			velocity = direction * SPEED
+	else:
+		velocity = Vector2.ZERO
 
 	move_and_slide()
