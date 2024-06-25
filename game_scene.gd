7f7df91dbcf3b6747f65d91f9751668c9eb3b7fb
OhijMik <jihok148@gmail.com>
2024-06-24 21:15:24 -0400

    Added the projectile spawn frequency buttons
    - Added the projectile spawn frequency buttons so that the player can change how frequently the projectiles are spawned

--- game_scene.gd
+++ game_scene.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var cursor_click = preload("res://cursor_click.tscn")
 @onready var champion = get_node("Champion")
+@onready var projectile_spawn_timer = get_node("ProjectileSpawn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +17,8 @@ func _process(delta):
 		var cursor_click_temp = cursor_click.instantiate()
 		cursor_click_temp.position = mouse_pos
 		add_child(cursor_click_temp)
+	
+	$UI/ProjectileFrequency/FrequencyLabel.text = str(projectile_spawn_timer.wait_time)
 
 
 func _on_h_slider_value_changed(value):
@@ -24,3 +27,11 @@ func _on_h_slider_value_changed(value):
 
 func _on_speed_slider_value_changed(value):
 	champion.speed = value + 150.0
+
+
+func _on_plus_button_pressed():
+	projectile_spawn_timer.wait_time += 0.05
+
+
+func _on_minus_button_pressed():
+	projectile_spawn_timer.wait_time -= 0.05
