3439b207b6ee6661b733ede4bdc712c6303aa543
OhijMik <jihok148@gmail.com>
2024-07-04 13:41:56 -0400

    Added the levels scene

--- codes/main_menu.gd
+++ codes/main_menu.gd
@@ -9,3 +9,7 @@ func _on_custom_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_levels_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels_scene.tscn")
