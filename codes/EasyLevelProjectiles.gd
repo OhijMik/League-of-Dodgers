5562dc6e511fee3d21b94a46ba80d487563f2c1a
OhijMik <jihok148@gmail.com>
2024-07-08 12:22:25 -0400

    Finished the easy level

--- codes/EasyLevelProjectiles.gd
+++ codes/EasyLevelProjectiles.gd
@@ -24,28 +24,28 @@ func _process(delta):
 
 
 func _on_projectile_spawn_timeout():
-	if curr_projectile != "falling":
-		for i in range(proj_amt):
-			var projectile_temp = projectile.instantiate()
-			var rng = RandomNumberGenerator.new()
-			var randInt = rng.randi_range(0, 3)
-			if randInt == 0:
-				projectile_temp.position = Vector2(rng.randi_range(0, 1150), 0)
-			elif randInt == 1:
-				projectile_temp.position = Vector2(rng.randi_range(0, 1150), 650)
-			elif randInt == 2:
-				projectile_temp.position = Vector2(0, rng.randi_range(0, 650))
-			else:
-				projectile_temp.position = Vector2(1150, rng.randi_range(0, 650))
-			
-			projectile_temp.speed = 300
-			projectile_temp.damage = 20
-			
-			add_child(projectile_temp)
+	var rng = RandomNumberGenerator.new()
+	var randProj = rng.randi_range(0, 1)
+	if randProj == 0:
+		curr_projectile = "small"
+		projectile = preload("res://scenes/small_projectile.tscn")
 	else:
+		curr_projectile = "medium"
+		projectile = preload("res://scenes/medium_projectile.tscn")
+	
+	for i in range(proj_amt):
 		var projectile_temp = projectile.instantiate()
-		if not champion.direction:
-			projectile_temp.position = champion.position
+		var randInt = rng.randi_range(0, 3)
+		if randInt == 0:
+			projectile_temp.position = Vector2(rng.randi_range(0, 1150), 0)
+		elif randInt == 1:
+			projectile_temp.position = Vector2(rng.randi_range(0, 1150), 650)
+		elif randInt == 2:
+			projectile_temp.position = Vector2(0, rng.randi_range(0, 650))
 		else:
-			projectile_temp.position = champion.position + ((champion.speed / 2.5) * champion.direction)
+			projectile_temp.position = Vector2(1150, rng.randi_range(0, 650))
+		
+		projectile_temp.speed = 300
+		projectile_temp.damage = 20
+		
 		add_child(projectile_temp)
